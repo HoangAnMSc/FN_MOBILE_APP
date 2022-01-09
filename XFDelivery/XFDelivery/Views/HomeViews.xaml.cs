ef99f248f0dc5018c71a086799b2713374556fb7
Hoàng An <hoanganmsc@gmail.com>
2022-01-09 07:54:58 +0700

    new UI

---
+++ XFDelivery/XFDelivery/Views/HomeViews.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XFDelivery.ViewModels;
+
+namespace XFDelivery.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomeViews : ContentPage
+    {
+        public HomeViews()
+        {
+            InitializeComponent();
+            BindingContext = new HomePageViewModels(Navigation);
+        }
+    }
+}
